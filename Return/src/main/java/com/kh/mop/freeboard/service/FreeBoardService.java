14a4022c2d31cf7bf48151967905255186ace754
MUJUN <MUJUN@MJ>
2020-12-09 03:46:23 +0900

    FreeBoardService interface

--- Return/src/main/java/com/kh/mop/freeboard/service/FreeBoardService.java
+++ Return/src/main/java/com/kh/mop/freeboard/service/FreeBoardService.java
@@ -4,20 +4,21 @@ import java.util.ArrayList;
 
 import com.kh.mop.freeboard.domain.FreeBoard;
 import com.kh.mop.freeboard.domain.FreeBoardPageInfo;
+import com.kh.mop.freeboard.domain.FreeBoardReply;
 
 public interface FreeBoardService {
 
-	/**
-	 * 게시판 전체 수 조회
-	 * @return
-	 */
+
 	public int getListCount();
-	
 	public ArrayList<FreeBoard> selectList(FreeBoardPageInfo pi);
 	public int addReadCount(int fId);
 	public FreeBoard selectFreeBoard(int fId);
 	public int updateFreeBoard(FreeBoard freeBoard);
 	public int deleteFreeBoard(FreeBoard freeBoard);
 	public int insertFreeBoard(FreeBoard freeBoard);
+	public ArrayList<FreeBoardReply> selectFreeBoardReplyList(int fId);
+	public int insertFreeBoardReply(FreeBoardReply freeBoardReply);
+	public int updateFreeBoardReply(FreeBoardReply freeBoardReply);
+	public int deleteFreeBoardReply(int fbrId);
 	
 }
